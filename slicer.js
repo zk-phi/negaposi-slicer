6c1ed439a102db105b1def249b7d3e0b8952c772
zk-phi <zk-phi@users.noreply.github.com>
2020-07-20 11:55:10 +0900

    Add feature to change the base color when creating a power map

--- slicer.js
+++ slicer.js
@@ -28,7 +28,7 @@ function generateImageData (blobUrl, callback) {
 }
 
 /* ImageData -> Float64Array */
-function imageDataToPowerArray (data) {
+function imageDataToPowerArray (data, baseColor) {
     var length = data.width * data.height;
     var arr = new Float64Array(length);
 
@@ -37,9 +37,9 @@ function imageDataToPowerArray (data) {
             arr[ix] = -1;
         } else {
             arr[ix] = Math.sqrt(
-                Math.pow(data.data[ix * 4 + 0], 2)
-                + Math.pow(data.data[ix * 4 + 1], 2)
-                + Math.pow(data.data[ix * 4 + 2], 2)
+                Math.pow(data.data[ix * 4 + 0] - baseColor[0], 2)
+                + Math.pow(data.data[ix * 4 + 1] - baseColor[1], 2)
+                + Math.pow(data.data[ix * 4 + 2] - baseColor[2], 2)
             ) / 255 / SQRT3;
         }
     }
@@ -68,6 +68,23 @@ function renderSlice (canvas, imageData, powerArray, slice) {
     canvas.getContext("2d").putImageData(new ImageData(arr, imageData.height, imageData.width), 0, 0);
 }
 
+function renderBaseImage (canvas, imageData, powerArray) {
+    canvas.width  = imageData.width;
+    canvas.height = imageData.height;
+
+    var length = powerArray.length;
+    var arr = new Uint8ClampedArray(length * 4);
+
+    for (var i = 0; i < length; i++) {
+        if (powerArray[i] >= 0) {
+            arr[i * 4 + 0] = arr[i * 4 + 1] = arr[i * 4 + 2] = (1 - powerArray[i]) * 255;
+            arr[i * 4 + 3] = 255;
+        }
+    }
+
+    canvas.getContext("2d").putImageData(new ImageData(arr, imageData.height, imageData.width), 0, 0);
+}
+
 /* ---- main */
 
 var SQRT3 = Math.sqrt(3);
@@ -83,10 +100,15 @@ var vm = new Vue({
             blobUrl: "",
             powerArray: null
         },
+        baseColor: [0, 0, 0],
         slice1: [[false, 0, 1.02]],
         slice2: [[false, 0, 1.02]]
     },
     watch: {
+        baseColor: {
+            handler: function () { vm.refreshPowerImage(); },
+            deep: true
+        },
         slice1: {
             handler: function () { vm.refreshSlice1(); },
             deep: true
@@ -103,12 +125,16 @@ var vm = new Vue({
                 generateImageData(blobUrl, function (imageData) {
                     vm.source.blobUrl = blobUrl;
                     vm.source.imageData = imageData;
-                    vm.source.powerArray = imageDataToPowerArray(imageData);
+                    vm.refreshPowerImage();
                     vm.refreshSlice1();
                     vm.refreshSlice2();
                     vm.status = STATUSES.LOADED;
                 });
             });
+        },
+        refreshPowerImage: function () {
+            vm.source.powerArray = imageDataToPowerArray(vm.source.imageData, vm.baseColor);
+            renderBaseImage(vm.$refs.canvasBase, vm.source.imageData, vm.source.powerArray);
         },
         refreshSlice1: function () {
             renderSlice(vm.$refs.canvas1, vm.source.imageData, vm.source.powerArray, vm.slice1);
